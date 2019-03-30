01cbf720136a895122f1c977685179af86b12f4e
Ahnaf Alfariza <alfariza.ahnaf10@gmail.com>
2019-03-30 21:39:24 +0700

    Add javascript

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@ var currentScrollPos = window.pageYOffset;
 	}
 	prevScrollpos = currentScrollPos;
 }
+
+
+function readMore(id1,id2,id3){
+	var dots = document.getElementById(id1);
+	var moreText = document.getElementById(id2);
+	var btnText = document.getElementById(id3);
+
+	if (dots.style.display === "none") {
+		dots.style.display = "inline";
+		btnText.innerHTML = "Read more"; 
+		moreText.style.display = "none";
+	} else {
+		dots.style.display = "none";
+		btnText.innerHTML = "Read less"; 
+		moreText.style.display = "inline";
+	}
+}
+
+document.getElementById("myBtn").onclick = function(){readMore("dots","more","myBtn")}
+document.getElementById("myBtn2").onclick = function(){readMore("dots2","more2","myBtn2")}
+document.getElementById("myBtn3").onclick = function(){readMore("dots3","more3","myBtn3")}
